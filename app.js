9478f0bd79d3d821c66771714c88b635c3648869
Gaonkar30 <akashggaonkar@gmail.com>
2024-07-12 12:07:23 +0530

    redis client done and weather data available from city name

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
-require('dotenv').config();
-const express=require('express');
-const axios=require('axios');
-const redis=require('redis');
-const app=express();
-const PORT=3000||process.env.PORT;
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
+const app = express();
+const PORT = 3000 || process.env.PORT;
 
 //redis client
-const redisclient=redis.createClient();
+const redisclient = redis.createClient();
+
+redisclient.on("error", (err) => {
+  console.log("redis error", err);
+});
+
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  const apiKey = process.env.VISUAL_CROSSING_API_KEY;
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`;
+
+  // check cache first
+  redisclient.get(city, async (err, cachedData) => {
+    if (err) {
+      console.log("error in redis", err);
+      return res.json("server error");
+    }
+    if (cachedData) {
+      res.json(JSON.parse(cachedData));
+    } else {
+      //fetch data otherwise
+      try {
+        const response = await axios.get(url);
+        const data = response.data;
+        //10 hr expiration on data stored in cache
+        redisclient.setEx(city, 36000, JSON.stringify(data));
+        res.json(data);
+      } catch (err) {
+        console.error("api error", err);
+        res.json("failed to fetch data");
+      }
+    }
+  });
+});
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
